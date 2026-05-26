b2e1fcfd126b36c2511daf19beefe8e586cc1d61
Abel Asaya <abelasaya@gmail.com>
2026-05-26 06:05:23 +0100

    fix socket cors

--- src/socket.ts
+++ src/socket.ts
@@ -8,7 +8,7 @@ const connectedUsers = new Map<string, string>();
 export const initSocket = (server: any) => {
   io = new Server(server, {
     cors: {
-      origin: "http://localhost:5173",
+      origin: ["http://localhost:5173", "https://celebrateflow.com"],
 
       methods: ["GET", "POST", "PUT"],
     },
